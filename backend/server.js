90236d35e7c8fb2387c9e0d6bc7c2c56aa684ed1
Papun1111 <gohanmohapatra@gmail.com>
2024-09-16 21:53:07 +0530

    new folder added

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 //app config
 const app=express();
@@ -7,12 +9,15 @@ const port=4000;
 //middleware
 app.use(express.json());
 app.use(cors());
-
+dotenv.config();
+const mongoUrl=process.env.MONGO_URL;
 app.get("/",(req,res)=>{
     res.send("hello world");
 })
 
-app.listen(port,()=>{
+app.listen(port,async()=>{
+    await mongoose.connect(mongoUrl);
+    
     console.log(`listening on port ${port}`);
 })
 
