e19b601660f2ca0e2ec15ae70c55d8c671d18d99
Papun1111 <gohanmohapatra@gmail.com>
2024-09-16 21:37:05 +0530

    backend set up

---
+++ backend/server.js
@@ -0,0 +1,40 @@
+import express from "express";
+import cors from "cors";
+
+//app config
+const app=express();
+const port=4000;
+//middleware
+app.use(express.json());
+app.use(cors());
+
+app.get("/",(req,res)=>{
+    res.send("hello world");
+})
+
+app.listen(port,()=>{
+    console.log(`listening on port ${port}`);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
