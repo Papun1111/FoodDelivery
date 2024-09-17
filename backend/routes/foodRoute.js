93490ffe60072b4ecbd685c519ba28fca5800a14
Papun1111 <gohanmohapatra@gmail.com>
2024-09-17 09:24:48 +0530

    storage functionality added

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -4,6 +4,14 @@ import { addFood } from "../controllers/foodController.js"
 import multer from "multer"
 const foodRouter=Router();
 //Image Storage engine
+const storage =multer.diskStorage({
+    destination:"uploads",
+    filename:(req,res,cb)=>{
+return cb(null,`${Date.now()}${file.originalname}`)
+    }
+})
+const upload=multer({storage:storage})
+
 foodRouter.post("/add",addFood);
 
 export default foodRouter;
