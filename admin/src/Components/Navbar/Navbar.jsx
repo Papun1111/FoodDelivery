c792abbaa4d02e576c6ab573287159205de363fa
Papun1111 <gohanmohapatra@gmail.com>
2024-09-19 19:32:29 +0530

    admin updated

--- admin/src/Components/Navbar/Navbar.jsx
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import "./Navbar.css"
+import {assets} from "../../assets/assets"
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className='navbar'>
+      <h1 className='logo'>Pamotra</h1>
+      <img src={assets.profile_image} className='profile' alt="" />
     </div>
   )
 }
