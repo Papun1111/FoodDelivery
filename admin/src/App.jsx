8c5598c8272b43422ce7bc950f03db94921dfad0
Papun1111 <gohanmohapatra@gmail.com>
2024-09-24 20:52:30 +0530

    url updated

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -8,7 +8,7 @@ import Orders from './pages/Orders/Orders'
 import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 const App = () => {
-  const url =`http://localhost:4000`;
+  const url =`https://fooddelivery-5wc2.onrender.com`;
   return (
     <div>
       <ToastContainer></ToastContainer>
